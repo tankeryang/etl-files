7fb9a0a4a4c90c06e100eb8d0c143d52b0e66c9e
yang <yang.yang@trendy-global.com>
2018-08-29 15:44:16 +0800

    mod: order_pay_item

--- sql/dwh/ods/crm/order_pay_item/fully.sql
+++ sql/dwh/ods/crm/order_pay_item/fully.sql
@@ -1,22 +1,3 @@
-CREATE SCHEMA IF NOT EXISTS ods_crm;
-
-
-DROP TABLE IF EXISTS ods_crm.order_pay_item;
-
-
-CREATE TABLE ods_crm.order_pay_item (
-  pay_item_id    INTEGER, -- primary key
-  order_id       INTEGER,
-  order_from     INTEGER,
-  outer_order_no VARCHAR,
-  pay_type       VARCHAR,
-  currency       VARCHAR,
-  pay_amount     DECIMAL(38, 2),
-  coupon_no      VARCHAR,
-  create_time    TIMESTAMP
-);
-
-
 INSERT INTO order_pay_item
   SELECT
     pay_item_id,
