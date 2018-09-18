205180e45055960ec5561530f8ef1971a83bfcd3
yang <yang.yang@trendy-global.com>
2018-09-18 17:20:56 +0800

    mod: nowbefore income

--- sql/dwh/ads/crm/member_analyse_ach_member_nowbefore_income/query.sql
+++ sql/dwh/ads/crm/member_analyse_ach_member_nowbefore_income/query.sql
@@ -1,12 +1,9 @@
--- test
+-- test sales_area
 
 WITH mab AS (
     SELECT DISTINCT
-        maba.country,
         maba.sales_area,
-        maba.sales_district,
         maba.order_channel,
-        maba.city,
         maba.brand_code,
         maba.store_code,
         maba.sales_mode,
@@ -58,12 +55,12 @@ SELECT DISTINCT
     cast(IF(IF(sm2.oa IS NOT NULL, sm2.oa, 0) != 0, IF(sm2.siq IS NOT NULL, sm2.siq, 0) / IF(sm2.oa IS NOT NULL, sm2.oa, 0), 0) AS INTEGER) AS sales_item_per_order
 FROM (
     SELECT DISTINCT
-        country, order_channel, sales_mode, store_type, store_level, channel_type, member_type
+        sales_area, order_channel, sales_mode, store_type, store_level, channel_type, member_type
     FROM cdm_crm.member_analyse_type_label
 ) matl2
 
 LEFT JOIN (
-    SELECT country, order_channel, sales_mode, store_type, store_level, channel_type, member_type,
+    SELECT sales_area, order_channel, sales_mode, store_type, store_level, channel_type, member_type,
     sum(mab.order_fact_amount) * 1.0 / 10000                  AS si,
     IF(member_type = '会员', count(distinct member_no), NULL)  AS ca,
     count(distinct mab.outer_order_no)                        AS oa,
@@ -71,9 +68,9 @@ LEFT JOIN (
     FROM mab
     WHERE date(mab.order_deal_time) <= date('2018-08-10')
     AND date(mab.order_deal_time) >= date('2018-01-01')
-    GROUP BY country, order_channel, sales_mode, store_type, store_level, channel_type, member_type
+    GROUP BY sales_area, order_channel, sales_mode, store_type, store_level, channel_type, member_type
 ) sm2
-ON matl2.country = sm2.country
+ON matl2.sales_area = sm2.sales_area
 AND matl2.order_channel = sm2.order_channel
 AND matl2.sales_mode = sm2.sales_mode
 AND matl2.store_type = sm2.store_type
@@ -82,14 +79,14 @@ AND matl2.channel_type = sm2.channel_type
 AND matl2.member_type = sm2.member_type
 
 LEFT JOIN (
-    SELECT country, order_channel, sales_mode, store_type, store_level, channel_type,
+    SELECT sales_area, order_channel, sales_mode, store_type, store_level, channel_type,
     sum(mab.order_fact_amount) *1.0 / 10000 AS ttsi
     FROM mab
     WHERE date(mab.order_deal_time) <= date('2018-08-10')
     AND date(mab.order_deal_time) >= date('2018-01-01')
-    GROUP BY country, order_channel, sales_mode, store_type, store_level, channel_type
+    GROUP BY sales_area, order_channel, sales_mode, store_type, store_level, channel_type
 ) smtt
-ON matl2.country = smtt.country
+ON matl2.sales_area = smtt.sales_area
 AND matl2.order_channel = smtt.order_channel
 AND matl2.sales_mode = smtt.sales_mode
 AND matl2.store_type = smtt.store_type
@@ -97,14 +94,14 @@ AND matl2.store_level = smtt.store_level
 AND matl2.channel_type = smtt.channel_type
 
 LEFT JOIN (
-    SELECT country, order_channel, sales_mode, store_type, store_level, channel_type, member_type,
+    SELECT sales_area, order_channel, sales_mode, store_type, store_level, channel_type, member_type,
     sum(mab.order_fact_amount) * 1.0 / 10000  AS si
     FROM mab
     WHERE date(mab.order_deal_time) <= date(date('2018-08-10') - interval '1' year)
     AND date(mab.order_deal_time) >= date(date('2018-01-01') - interval '1' year)
-    GROUP BY country, order_channel, sales_mode, store_type, store_level, channel_type, member_type
+    GROUP BY sales_area, order_channel, sales_mode, store_type, store_level, channel_type, member_type
 ) lyst2
-ON matl2.country = lyst2.country
+ON matl2.sales_area = lyst2.sales_area
 AND matl2.order_channel = lyst2.order_channel
 AND matl2.sales_mode = lyst2.sales_mode
 AND matl2.store_type = lyst2.store_type
@@ -112,10 +109,10 @@ AND matl2.store_level = lyst2.store_level
 AND matl2.channel_type = lyst2.channel_type
 AND matl2.member_type = lyst2.member_type
 
-WHERE matl2.country IN ('中国')
+WHERE matl2.sales_area IN ('华南')
 AND matl2.order_channel IN ('线下')
 AND matl2.sales_mode IN ('正价')
-AND matl2.store_type IN ('MALL')
+AND matl2.store_type IN ('百货')
 AND matl2.store_level IN ('A')
 AND matl2.channel_type IN ('自营')
 AND matl2.member_type = '会员'
@@ -141,12 +138,12 @@ cast(IF(IF(sm.siq IS NOT NULL, sm.siq, 0) != 0, IF(sm.si IS NOT NULL, sm.si, 0) 
 cast(IF(IF(sm.oa IS NOT NULL, sm.oa, 0) != 0, IF(sm.siq IS NOT NULL, sm.siq, 0) / IF(sm.oa IS NOT NULL, sm.oa, 0), 0) AS INTEGER) AS sales_item_per_order
 FROM (
     SELECT DISTINCT
-    country, order_channel, sales_mode, store_type, store_level, channel_type, member_nowbefore_type
+    sales_area, order_channel, sales_mode, store_type, store_level, channel_type, member_nowbefore_type
     FROM cdm_crm.member_analyse_type_label
 ) matl
 
 LEFT JOIN (
-    SELECT country, order_channel, sales_mode, store_type, store_level, channel_type, member_nowbefore_type,
+    SELECT sales_area, order_channel, sales_mode, store_type, store_level, channel_type, member_nowbefore_type,
     sum(mab.order_fact_amount) * 1.0 / 10000 AS si,
     count(distinct mab.member_no)            AS ca,
     count(distinct mab.outer_order_no)       AS oa,
@@ -154,9 +151,9 @@ LEFT JOIN (
     FROM mab
     WHERE date(mab.order_deal_time) <= date('2018-08-10')
     AND date(mab.order_deal_time) >= date('2018-01-01')
-    GROUP BY country, order_channel, sales_mode, store_type, store_level, channel_type, member_nowbefore_type
+    GROUP BY sales_area, order_channel, sales_mode, store_type, store_level, channel_type, member_nowbefore_type
 ) sm
-    ON matl.country = sm.country
+    ON matl.sales_area = sm.sales_area
     AND matl.order_channel = sm.order_channel
     AND matl.sales_mode = sm.sales_mode
     AND matl.store_type = sm.store_type
@@ -165,14 +162,14 @@ LEFT JOIN (
     AND matl.member_nowbefore_type = sm.member_nowbefore_type
 
 LEFT JOIN (
-    SELECT country, order_channel, sales_mode, store_type, store_level, channel_type,
+    SELECT sales_area, order_channel, sales_mode, store_type, store_level, channel_type,
     sum(mab.order_fact_amount) * 1.0 / 10000 AS ttsi
     FROM mab
     WHERE date(mab.order_deal_time) <= date('2018-08-10')
     AND date(mab.order_deal_time) >= date('2018-01-01')
-    GROUP BY country, order_channel, sales_mode, store_type, store_level, channel_type
+    GROUP BY sales_area, order_channel, sales_mode, store_type, store_level, channel_type
 ) smtt
-ON matl.country = smtt.country
+ON matl.sales_area = smtt.sales_area
 AND matl.order_channel = smtt.order_channel
 AND matl.sales_mode = smtt.sales_mode
 AND matl.store_type = smtt.store_type
@@ -180,14 +177,14 @@ AND matl.store_level = smtt.store_level
 AND matl.channel_type = smtt.channel_type
 
 LEFT JOIN (
-    SELECT country, order_channel, sales_mode, store_type, store_level, channel_type, member_nowbefore_type,
+    SELECT sales_area, order_channel, sales_mode, store_type, store_level, channel_type, member_nowbefore_type,
     sum(mab.order_fact_amount) * 1.0 / 10000  AS si
     FROM mab
     WHERE date(mab.order_deal_time) <= date(date('2018-08-10') - interval '1' year)
     AND date(mab.order_deal_time) >= date(date('2018-01-01') - interval '1' year)
-    GROUP BY country, order_channel, sales_mode, store_type, store_level, channel_type, member_nowbefore_type
+    GROUP BY sales_area, order_channel, sales_mode, store_type, store_level, channel_type, member_nowbefore_type
 ) lyst
-ON matl.country = lyst.country
+ON matl.sales_area = lyst.sales_area
 AND matl.order_channel = lyst.order_channel
 AND matl.sales_mode = lyst.sales_mode
 AND matl.store_type = lyst.store_type
@@ -195,9 +192,9 @@ AND matl.store_level = lyst.store_level
 AND matl.channel_type = lyst.channel_type
 AND matl.member_nowbefore_type = lyst.member_nowbefore_type
 
-WHERE matl.country IN ('中国')
+WHERE matl.sales_area IN ('华南')
 AND matl.order_channel IN ('线下')
 AND matl.sales_mode IN ('正价')
-AND matl.store_type IN ('MALL')
+AND matl.store_type IN ('百货')
 AND matl.store_level IN ('A')
 AND matl.channel_type IN ('自营');
