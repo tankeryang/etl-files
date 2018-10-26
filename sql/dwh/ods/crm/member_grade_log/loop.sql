35264cd7744c42ca9767c3881b8a5ef3057ca815
yang <yang.yang@trendy-global.com>
2018-10-26 14:37:24 +0800

    mod: 添加brand_code

--- sql/dwh/ods/crm/member_grade_log/loop.sql
+++ sql/dwh/ods/crm/member_grade_log/loop.sql
@@ -1,16 +1,16 @@
 INSERT INTO ods_crm.member_grade_log
     SELECT
-        log_id                    AS log_id,
-        member_no                 AS member_no,
-        before_grade_id           AS before_grade_id,
-        after_grade_id            AS current_grade_id,
-        grade_change_time         AS grade_change_time,
-        localtimestamp            AS create_time
-    FROM
-        dev_mysql_fpsit.crm.member_grade_log
+        log_id            AS log_id,
+        member_no         AS member_no,
+        brand_code        AS brand_code,
+        before_grade_id   AS before_grade_id,
+        after_grade_id    AS current_grade_id,
+        grade_change_time AS grade_change_time,
+        localtimestamp    AS create_time
+    FROM dev_mysql_fpsit.crm.member_grade_log
     WHERE
-        date_format(grade_change_time, '%Y-%m-%d %T') > (
+        date_format(create_time, '%Y-%m-%d %T') > (
             SELECT date_format(max(create_time), '%Y-%m-%d %T')
             FROM ods_crm.member_grade_log
         )
-        AND date_format(grade_change_time, '%Y-%m-%d %T') <= date_format(localtimestamp, '%Y-%m-%d %T');
+    AND date(create_time) < date(localtimestamp);
