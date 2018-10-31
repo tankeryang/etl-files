20bd3ec0d0054d6327fafa3fae580dd4e2feee42
yang <yang.yang@trendy-global.com>
2018-10-31 16:52:43 +0800

    mod: ods_crm.member_info_brand_id

--- sql/dwh/ods/crm/member_info_brand_id/create.sql
+++ sql/dwh/ods/crm/member_info_brand_id/create.sql
@@ -5,7 +5,9 @@ DROP TABLE IF EXISTS ods_crm.member_info_brand_id;
 
 
 CREATE TABLE ods_crm.member_info_brand_id (
-    member_no  VARCHAR,
-    brand_code VARCHAR,
-    brand_id   INTEGER
+    member_no   VARCHAR,
+    brand_code  VARCHAR,
+    brand_id    INTEGER,
+    create_time TIMESTAMP,
+    modify_time TIMESTAMP
 );
--- sql/dwh/ods/crm/member_info_brand_id/fully.sql
+++ sql/dwh/ods/crm/member_info_brand_id/fully.sql
@@ -5,7 +5,9 @@ INSERT INTO ods_crm.member_info_brand_id
     SELECT
         cast(a.user_id AS VARCHAR) AS member_no,
         '2'                        AS brand_code,
-        d.brand_id                 AS brand_id
+        d.brand_id                 AS brand_id,
+        d.create_time              AS create_time,
+        d.modify_time              AS modify_time
         FROM prod_remote_mysql_crm.ec_user.user_reg a
         INNER JOIN prod_remote_mysql_crm.ec_user.user_info b ON a.user_id = b.user_id
         INNER JOIN prod_remote_mysql_crm.ec_loyalty.user_profile c ON a.user_id = c.user_id AND c.store_id = 2
@@ -14,7 +16,9 @@ INSERT INTO ods_crm.member_info_brand_id
     UNION SELECT
         cast(a.user_id AS VARCHAR) AS member_no,
         '6'                        AS brand_code,
-        d.brand_id                 AS brand_id
+        d.brand_id                 AS brand_id,
+        d.create_time              AS create_time,
+        d.modify_time              AS modify_time
         FROM prod_remote_mysql_crm.ec_user.user_reg a
         INNER JOIN prod_remote_mysql_crm.ec_user.user_info b ON a.user_id = b.user_id
         INNER JOIN prod_remote_mysql_crm.ec_loyalty.user_profile c ON a.user_id = c.user_id AND c.store_id = 23
