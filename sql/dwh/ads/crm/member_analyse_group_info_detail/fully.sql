1acca9311b6d034ffc269bd4c43b9197432d801e
yang <yang.yang@trendy-global.com>
2018-11-05 20:00:32 +0800

    add: 分组的etl

--- sql/dwh/ads/crm/member_analyse_group_info_detail/fully.sql
+++ sql/dwh/ads/crm/member_analyse_group_info_detail/fully.sql
@@ -20,6 +20,7 @@ INSERT INTO ads_crm.member_analyse_group_info_detail
         mfo.order_fact_amount AS first_order_fact_amount,
         cast(day(date(localtimestamp) - date(mfo.order_deal_time)) AS INTEGER) AS first_order_deal_time_gap_with_today,
         -- 产品属性偏好
+        
     FROM cdm_crm.member_info_detail mid
     LEFT JOIN cdm_crm.member_last_order mlo ON mid.brand_code = mlo.brand_code AND mid.member_no = mlo.member_no
 
--- sql/dwh/cdm/crm/member_consumption_preference/fully.sql
+++ sql/dwh/cdm/crm/member_consumption_preference/fully.sql
@@ -10,4 +10,28 @@ INSERT INTO cdm_crm.member_consumption_preference
             count(store_code) OVER (PARTITION BY brand_code, member_no, store_code) AS store_code_count
         FROM cdm_crm.order_info_detail
         WHERE COALESCE(try_cast(member_no AS INTEGER), 0) > 0
-    )
+    ), sp AS (
+        SELECT DISTINCT
+            brand_code,
+            member_no,
+            first_value(store_code) OVER (
+                PARTITION BY brand_code, member_no
+                ORDER BY store_code_count DESC) AS store_preference
+        FROM spc
+    ), ppc AS (
+        SELECT DISTINCT
+            brand_code,
+            member_no,
+            order_pay_type,
+            count(order_pay_type) OVER (PARTITION BY brand_code, member_no, order_pay_type) AS pay_type_count
+        FROM cdm_crm.order_info_detail
+        WHERE COALESCE(try_cast(member_no AS INTEGER), 0) > 0
+    ), pp AS (
+        SELECT DISTINCT
+            brand_code,
+            member_no,
+            first_value(order_pay_type) OVER (
+                PARTITION BY brand_code, member_no
+                ORDER BY pay_type_count DESC) AS pay_type_preference
+        FROM ppc
+    ), 
