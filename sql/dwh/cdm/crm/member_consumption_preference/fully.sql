c7238bcca5318add05a1b94ed9c2df17a0aa7d99
yang <yang.yang@trendy-global.com>
2018-11-13 19:29:58 +0800

    fix: sql

--- sql/dwh/cdm/crm/member_consumption_preference/fully.sql
+++ sql/dwh/cdm/crm/member_consumption_preference/fully.sql
@@ -80,6 +80,7 @@ INSERT INTO cdm_crm.member_consumption_preference
             brand_code,
             member_no,
             cast(sum(order_item_auantity) / count(outer_order_no) AS DECIMAL(18,4)) AS related_rate
+        FROM cdm_crm.order_info_detail
         WHERE COALESCE(try_cast(member_no AS INTEGER), 0) > 0
         AND date(order_deal_time) <= date(localtimestamp)
         AND date(order_deal_time) >= date(localtimestamp) - INTERVAL '{computing_duration}' day
