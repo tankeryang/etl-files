f4f074afef0964faee1410c318fbb64a9396d32f
yang <yang.yang@trendy-global.com>
2018-11-20 16:37:15 +0800

    mod: cic static

--- sql/dwh/ads/crm/member_analyse_income_total_zone_weekly_all/fully-placeholders.sql
+++ sql/dwh/ads/crm/member_analyse_income_total_zone_weekly_all/fully-placeholders.sql
@@ -1,1 +1,9 @@
-SELECT array['country', 'sales_area', 'sales_district', 'province', 'city'] AS zone;
+WITH g AS (
+    SELECT cast(dow(localtimestamp) AS VARCHAR) AS gap
+), gg AS (
+    SELECT date(localtimestamp) - interval g.gap day AS week_head_date FROM g
+)
+SELECT
+    array['country', 'sales_area', 'sales_district', 'province', 'city'] AS zone,
+    array[gg.week_head_date] AS gap
+FROM gg;
