ab66c34be33e6cbcd937e400bd162a0923fb49c8
yang <yang.yang@trendy-global.com>
2018-11-21 10:52:28 +0800

    mod: member_analyse_income_total_zone_all

--- sql/dwh/ads/crm/member_analyse_income_total_zone_all/create.sql
+++ sql/dwh/ads/crm/member_analyse_income_total_zone_all/create.sql
@@ -1,10 +1,10 @@
 CREATE SCHEMA IF NOT EXISTS ads_crm;
 
 
-DROP TABLE IF EXISTS ads_crm.member_analyse_income_total_zone_daily_all;
+DROP TABLE IF EXISTS ads_crm.member_analyse_income_total_zone_all;
 
 
-CREATE TABLE IF NOT EXISTS ads_crm.member_analyse_income_total_zone_daily_all (
+CREATE TABLE IF NOT EXISTS ads_crm.member_analyse_income_total_zone_all (
     brand                   VARCHAR,
     order_channel           VARCHAR,
     zone                    VARCHAR,
@@ -20,5 +20,6 @@ CREATE TABLE IF NOT EXISTS ads_crm.member_analyse_income_total_zone_daily_all (
     sales_item_per_order    DECIMAL(18, 2),
     compared_with_lyst      DECIMAL(18, 4),
     compared_with_ss_lyst   DECIMAL(18, 4),
+    duration_type           VARCHAR,
     create_time             TIMESTAMP
 );
--- sql/dwh/ads/crm/member_analyse_income_total_zone_all/fully-order_channel-all-daily.sql
+++ sql/dwh/ads/crm/member_analyse_income_total_zone_all/fully-order_channel-all-daily.sql
@@ -1,10 +1,9 @@
-INSERT INTO ads_crm.member_analyse_income_total_zone_monthly_all
+INSERT INTO ads_crm.member_analyse_income_total_zone_all
     WITH tt AS (
         SELECT brand_name, {zone}, cast(sum(sales_income) AS DECIMAL(18, 3)) AS sales_income
         FROM ads_crm.member_analyse_fold_daily_income_detail
         WHERE member_type = '整体' AND member_newold_type IS NULL AND member_level_type IS NULL
-        AND date <= date(localtimestamp)
-        AND date >= date(date_format(localtimestamp, '%Y-%m-01'))
+        AND date = date(localtimestamp)
         GROUP BY brand_name, {zone}
     ), lyst AS (
         SELECT brand_name, {zone}, member_type,
@@ -12,8 +11,7 @@ INSERT INTO ads_crm.member_analyse_income_total_zone_monthly_all
             array_distinct(array_agg(store_code)) AS store_array
         FROM ads_crm.member_analyse_fold_daily_income_detail
         WHERE member_type IS NOT NULL AND member_newold_type IS NULL AND member_level_type IS NULL
-        AND date <= date(date(localtimestamp) - interval '1' year)
-        AND date >= date(date(date_format(localtimestamp, '%Y-%m-01')) - interval '1' year)
+        AND date = date(date(localtimestamp) - interval '1' year)
         GROUP BY brand_name, {zone}, member_type
     ), ss AS (
         SELECT ss.brand_name, ss.{zone}, ss.member_type,
@@ -23,7 +21,6 @@ INSERT INTO ads_crm.member_analyse_income_total_zone_monthly_all
         AND ss.{zone} = lyst.{zone} AND ss.member_type = lyst.member_type
         WHERE ss.member_type IS NOT NULL AND ss.member_newold_type IS NULL AND ss.member_level_type IS NULL
         AND ss.date <= date(localtimestamp)
-        AND ss.date >= date(date_format(localtimestamp, '%Y-%m-01'))
         GROUP BY ss.brand_name, ss.{zone}, ss.member_type, lyst.store_array
     ), ss_lyst AS (
         SELECT ss_l.brand_name, ss_l.{zone}, ss_l.member_type,
@@ -33,8 +30,7 @@ INSERT INTO ads_crm.member_analyse_income_total_zone_monthly_all
         AND ss_l.{zone} = ss.{zone} AND ss_l.member_type = ss.member_type
         WHERE ss_l.member_type IS NOT NULL AND ss_l.member_newold_type IS NULL AND ss_l.member_level_type IS NULL
         AND contains(ss.store_array, ss_l.store_code)
-        AND ss_l.date <= date(date(localtimestamp) - interval '1' year)
-        AND ss_l.date >= date(date(date_format(localtimestamp, '%Y-%m-01')) - interval '1' year)
+        AND ss_l.date = date(date(localtimestamp) - interval '1' year)
         GROUP BY ss_l.brand_name, ss_l.{zone}, ss_l.member_type
     ), ss_now AS (
         SELECT DISTINCT f.brand_name, f.{zone}, f.member_type,
@@ -46,8 +42,7 @@ INSERT INTO ads_crm.member_analyse_income_total_zone_monthly_all
         AND f.{zone} = ss.{zone} AND f.member_type = ss.member_type
         WHERE f.member_type IS NOT NULL AND f.member_newold_type IS NULL AND f.member_level_type IS NULL
         AND contains(ss.store_array, f.store_code)
-        AND f.date <= date(localtimestamp)
-        AND f.date >= date(date_format(localtimestamp, '%Y-%m-01'))
+        AND f.date = date(localtimestamp)
         GROUP BY f.brand_name, f.{zone}, f.member_type, ss_lyst.sales_income
     )
     SELECT DISTINCT
@@ -66,6 +61,7 @@ INSERT INTO ads_crm.member_analyse_income_total_zone_monthly_all
         cast(COALESCE(TRY(sum(f.sales_item_quantity) * 1.0 / sum(f.order_amount)), 0) AS DECIMAL(18, 2)) AS sales_item_per_order,
         cast(COALESCE(TRY(sum(f.sales_income) * 1.0 / lyst.sales_income), 0) AS DECIMAL(18, 4)) AS compared_with_lyst,
         cast(COALESCE(ss_now.compared_with_ss_lyst, 0) AS DECIMAL(18, 4)) AS compared_with_ss_lyst,
+        'daily' AS duration_type,
         localtimestamp AS create_time
     FROM ads_crm.member_analyse_fold_daily_income_detail f
     LEFT JOIN tt ON f.brand_name = tt.brand_name AND f.{zone} = tt.{zone}
@@ -74,5 +70,5 @@ INSERT INTO ads_crm.member_analyse_income_total_zone_monthly_all
     LEFT JOIN ss_now ON f.brand_name = ss_now.brand_name
     AND f.{zone} = ss_now.{zone} AND f.member_type = ss_now.member_type
     WHERE f.member_type IS NOT NULL AND f.member_newold_type IS NULL AND f.member_level_type IS NULL
-    AND f.date <= date(localtimestamp) AND f.date >= date(date_format(localtimestamp, '%Y-%m-01'))
+    AND f.date = date(localtimestamp)
     GROUP BY f.brand_name, f.{zone}, f.member_type, tt.sales_income, lyst.sales_income, ss_now.compared_with_ss_lyst;
