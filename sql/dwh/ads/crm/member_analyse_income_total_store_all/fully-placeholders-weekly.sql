7e99903d45055449dffdd2a59a8128257c69d325
yang <yang.yang@trendy-global.com>
2018-11-30 16:14:16 +0800

    add: store static sql

--- sql/dwh/ads/crm/member_analyse_income_total_store_all/fully-placeholders-weekly.sql
+++ sql/dwh/ads/crm/member_analyse_income_total_store_all/fully-placeholders-weekly.sql
@@ -2,6 +2,6 @@ WITH g AS (
     SELECT date(date_add('day', -dow(date(localtimestamp) - interval '1' day), localtimestamp)) AS first_date_of_week
 )
 SELECT
-    array['country', 'sales_area', 'sales_district', 'province', 'city'] AS zone,
+    array['store_code'] AS zone,
     array[g.first_date_of_week] AS first_date_of_week
 FROM g;
