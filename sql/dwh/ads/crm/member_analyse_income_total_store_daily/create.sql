fea047e76a5708ccec7d3d710f8a9ab3c2033460
yang <yang.yang@trendy-global.com>
2018-12-03 11:09:59 +0800

    add: store daily monthly

---
+++ sql/dwh/ads/crm/member_analyse_income_total_store_daily/create.sql
@@ -0,0 +1,22 @@
+CREATE SCHEMA IF NOT EXISTS ads_crm;
+
+
+DROP TABLE IF EXISTS ads_crm.member_analyse_income_total_store_daily;
+
+
+CREATE TABLE IF NOT EXISTS ads_crm.member_analyse_income_total_store_daily (
+    brand                   VARCHAR,
+    zone                    VARCHAR,
+    member_type             VARCHAR,
+    order_channel           VARCHAR,
+    sales_mode              VARCHAR,
+    store_type              VARCHAR,
+    store_level             VARCHAR,
+    channel_type            VARCHAR,
+    sales_income            DECIMAL(18, 3),
+    sales_income_proportion DECIMAL(18, 4),
+    compared_with_lyst      DECIMAL(18, 4),
+    compared_with_ss_lyst   DECIMAL(18, 4),
+    date                    DATE,
+    create_time             TIMESTAMP
+)
