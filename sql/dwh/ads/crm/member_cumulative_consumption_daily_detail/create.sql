f03812d8107e9eaa4219a2425aa23720b33718fe
yang <yang.yang@trendy-global.com>
2019-01-10 14:23:16 +0800

    add: ads_crm.member_cumulative_consumption_daily_detail

---
+++ sql/dwh/ads/crm/member_cumulative_consumption_daily_detail/create.sql
@@ -0,0 +1,20 @@
+CREATE SCHEMA IF NOT EXISTS ads_crm;
+
+
+DROP TABLE IF EXISTS ads_crm.member_cumulative_consumption_daily_detail;
+
+
+CREATE TABLE IF NOT EXISTS ads_crm.member_cumulative_consumption_daily_detail (
+    member_no                             VARCHAR,
+    brand_code                            VARCHAR,
+    cml_consumption_store                 ARRAY<VARCHAR>,
+    cml_consumption_times                 INTEGER,
+    cml_consumption_retail_amount         DECIMAL(18, 2),
+    cml_consumption_amount                DECIMAL(18, 2),
+    cml_consumption_amount_include_coupon DECIMAL(18, 2),
+    cml_return_times                      INTEGER,
+    cml_return_amount                     DECIMAL(18, 2),
+    cml_order_deal_date                   DATE,
+    vchr_cml_order_deal_year_month        VARCHAR,
+    vchr_cml_order_deal_date              VARCHAR
+) WITH (partitioned_by = ARRAY['vchr_cml_order_deal_year_month', 'vchr_cml_order_deal_date']);
---
+++ sql/dwh/ads/crm/member_cumulative_consumption_daily_detail/fully.sql
@@ -0,0 +1,24 @@
+DELETE FROM ads_crm.member_cumulative_consumption_daily_detail;
+
+
+INSERT INTO ads_crm.member_cumulative_consumption_daily_detail
+    SELECT
+        member_no,
+        brand_code,
+        ARRAY_DISTINCT(ARRAY_AGG(store_code)),
+        CAST(COUNT(DISTINCT consumption_order_no) AS INTEGER),
+        CAST(SUM(retail_amount) AS DECIMAL(18 ,2)),
+        CAST(SUM(consumption_amount) AS DECIMAL(18 ,2)),
+        CAST(SUM(consumption_amount_include_coupon) AS DECIMAL(18 ,2)),
+        CAST(COUNT(DISTINCT return_order_no) AS INTEGER),
+        CAST(SUM(return_amount) AS DECIMAL(18, 2)),
+        consumption_date,
+        DATE_FORMAT(consumption_date, '%Y-%m'),
+        DATE_FORMAT(consumption_date, '%Y-%m-%d')
+    FROM cdm_crm.member_cumulative_consumption
+    WHERE (
+        (consumption_order_no IS NOT NULL AND return_order_no IS NOT NULL) OR
+        (consumption_order_no IS NULL AND return_order_no IS NOT NULL) OR
+        (consumption_order_no IS NOT NULL AND return_order_no IS NULL)
+    )
+    GROUP BY member_no, brand_code, consumption_date;
